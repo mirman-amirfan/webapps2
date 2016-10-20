0e85b9307f02414c95294089e07f7b4dfc583d80
mirman-amirfan <amirfan2017@amirfan2017.local>
2016-10-20 14:37:05 -0700

    finished first objective

--- 2-the-fizzbuzz-event/app.js
+++ 2-the-fizzbuzz-event/app.js
@@ -1,6 +1,29 @@
-// Your Code Here:
-function fizzBuzz(n) {
-  // Implement FizzBuzz
-}
+$(document).ready(function(){
+$("#done").click(function() {
+    console.log("Running");
+    var f = parseInt($("#fizzFactor")
+        .val());
+    var b = parseInt($("#buzzFactor")
+        .val());
+    var n = parseInt($("#number")
+        .val());
 
-// Any other functions your app requires
+    console.log(f, b, n);
+
+    for (var i = 1; i < n+1; i++) {
+        var newDiv = $(document.createElement("div"));
+        if (i % (f * b) == 0) {
+            newDiv.append("FizzBuzz!");
+        } else if (i % b == 0) {
+            newDiv.append("Buzz");
+            newDiv.addClass("buzz");
+        } else if (i % f == 0) {
+            newDiv.append("Fizz!");
+        } else {
+            newDiv.append(i);
+        }
+        $("#output").append(newDiv);
+      }
+});
+
+});
